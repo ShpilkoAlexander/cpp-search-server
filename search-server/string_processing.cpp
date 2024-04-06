6e607a2095dcff46604de811e8861da49c3ca2ed
ShpilkoAlexander <shpilko.alexander@gmail.com>
2024-04-06 10:53:16 +0300

    Update string_processing.cpp

--- search-server/string_processing.cpp
+++ search-server/string_processing.cpp
@@ -3,27 +3,6 @@
 #include <string>
 #include <string_view>
 
-
-//std::vector<std::string> SplitIntoWords(const std::string& text) {
-//    std::vector<std::string> words;
-//    std::string word;
-//    for (const char c : text) {
-//        if (c == ' ') {
-//            if (!word.empty()) {
-//                words.push_back(word);
-//                word.clear();
-//            }
-//        } else {
-//            word += c;
-//        }
-//    }
-//    if (!word.empty()) {
-//        words.push_back(word);
-//    }
-
-//    return words;
-//}
-
 std::vector<std::string_view> SplitIntoWords(std::string_view str) {
     std::vector<std::string_view> result;
     int64_t pos = str.find_first_not_of(" ");
@@ -37,23 +16,3 @@ std::vector<std::string_view> SplitIntoWords(std::string_view str) {
 
     return result;
 }
-
-//std::vector<std::string> SplitIntoWords(std::string_view text) {
-//    std::vector<std::string> words;
-//    std::string word;
-//    for (const char c : text) {
-//        if (c == ' ') {
-//            if (!word.empty()) {
-//                words.push_back(word);
-//                word.clear();
-//            }
-//        } else {
-//            word += c;
-//        }
-//    }
-//    if (!word.empty()) {
-//        words.push_back(word);
-//    }
-
-//    return words;
-//}
